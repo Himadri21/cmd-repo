27f9357145f990f2c1b77210e6b2a6458b5bcf29
Himadri21 <himadri.khanchandani@gmail.com>
2020-09-17 20:57:18 +0530

    Inital commit

---
+++ src/main/java/com/example/demo/CmdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdExampleApplication.class, args);
+	}
+
+}
